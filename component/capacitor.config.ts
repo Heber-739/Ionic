536ac54993c99d0beb7bc9d2d4d400fee8c6fa0d
Heber Duarte <Heberduarteryr@gmail.com>
2024-12-15 09:41:15 -0300

    Reestructuracion de carpetas

--- component/capacitor.config.ts
+++ component/capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'my.app',
-  appName: 'my-app',
+  appId: 'io.ionic.starter',
+  appName: 'component',
   webDir: 'www'
 };
 
